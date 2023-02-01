4e4ec87ee1f0a9d84b6616920c4d600c5fcf2709
Daniel S Goncalves <dsg1407@gmail.com>
2023-02-01 15:43:06 -0300

    first calculator structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import "./styles/global.css"
 
 export function App() {
   return (
-    <>
-      <h1 className="text-purple-700 font-extrabold text-lg">Hello world!</h1>
-    </>
+    <div className="bg-gradient-to-r from-background-gradient-1 to-background-gradient-2 h-screen flex items-center justify-center ">
+      <main className="flex flex-col gap-6 pt-14 pb-8 px-8 bg-background-calculator rounded-6xl w-96 text-zinc-200 font-rubik">
+        <section>Calculo</section>
+        <section>Teclado</section>
+      </main>
+    </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+        rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        background: "#1A1924",
+        "background-gradient-1": "#8E7ECE",
+        "background-gradient-2": "#807ECE",
+        "background-calculator": "#2D2A37",
+      },
+      borderRadius: {
+        "6xl": "3rem",
       },
       gridTemplateRows: {
         layout: "76px 1fr 56px",
